ae05c09770eada2c1c85621c7252054a8d62b765
Laura Telfer <telfer5676@gmail.com>
2025-06-19 08:25:46 +0100

    Add light mode backgrounds

--- resources/views/livewire/shopping-summary.blade.php
+++ resources/views/livewire/shopping-summary.blade.php
@@ -1,17 +1,17 @@
 <div>
     <div class="grid grid-flow-col grid-columns-3 gap-4 mb-6">
-        <div class="flex items-center justify-between rounded-xl bg-white/10 border border-white/10 p-6">
+        <div class="flex items-center justify-between rounded-xl bg-gray-200/10 dark:bg-white/10 border border-gray-700/10 dark:border-white/10 p-6">
             <flux:heading>Total budget</flux:heading>
             <flux:text>£{{$budget}}</flux:text>
         </div>
 
-        <div class="flex items-center justify-between rounded-xl bg-white/10 border border-white/10 p-6">
+        <div class="flex items-center justify-between rounded-xl bg-gray-200/10 dark:bg-white/10 border border-gray-700/10 dark:border-white/10 p-6">
             <flux:heading>Total cost</flux:heading>
             <flux:text>£{{$total}}</flux:text>
         </div>
 
         <!-- change display for budget here -->
-        <div class="flex items-center justify-between rounded-xl bg-white/10 border border-white/10 p-6">
+        <div class="flex items-center justify-between rounded-xl bg-gray-200/10 dark:bg-white/10 border border-gray-700/10 dark:border-white/10 p-6">
             <flux:heading>Total difference</flux:heading>
             
             @if($difference < 0)
